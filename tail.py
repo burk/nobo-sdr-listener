183a227eab068ba290d86dc10704e9d600f105ef
Bjørn Rustad <bjorn@rustad.me>
2023-12-11 08:02:48 +0100

    Catch

--- tail.py
+++ tail.py
@@ -28,9 +28,9 @@ def main(filename: str, broker: str | None, port: int) -> None:
         while True:
             line = f.readline()
             if line is not None and line != "":
-                tokens = line.split(",")
-                data = tokens[2][5:]
                 try:
+                    tokens = line.split(",")
+                    data = tokens[2][5:]
                     packet = Packet.from_hex(data)
                 except Exception:
                     continue
@@ -38,7 +38,7 @@ def main(filename: str, broker: str | None, port: int) -> None:
                 if client:
                     client.publish(f"{TOPIC}/{packet.type_str()}/{packet.from_addr()}", packet.to_json())
             else:
-                time.sleep(0.2)
+                time.sleep(0.5)
     if client:
         client.loop_stop()
 
